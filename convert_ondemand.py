7494dc0852038bb4ca28a3f87f973ed0c37e746e
Jasper Ginn <jasperginn@hotmail.com>
2015-11-18 13:02:33 +0100

    initial commit

---
+++ convert_ondemand.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+
+'''
+Convert the on-demand course exports to a sqlite table
+'''
+
+import bs4 as BeautifulSoup
+import convert_ondemand_config as config
+import sqlite3
+import os
+import pandas
+from time import localtime, strftime
+
+'''
+
+This script takes on-demand data exports from coursera and dumps it in a sqlite database.
+
+You need to create two folders:
+	1) Folder with html files provided with the exports
+	2) Folder with csv files provided with the exports
+
+Specify the file paths to these folders in the 'convert_ondemand_config.py' file 
+
+The third line in the config file specifies the location and name of the sqlite database.
+
+	Example: "/home/jasper/Desktop/federalism.db" <-- Do not forget the '.db'.
+
+The fourth line specifies the location of a simple file for logging
+
+Run this script using the command 'python convert_ondemand.py'
+
+'''
+
+class scraper:
+
+	def __init__(self, fileN):
+		self.file = fileN
+
+	def scrape(self):
+		# Soup page
+		soup = BeautifulSoup.BeautifulSoup(open(self.file).read(), "html.parser")
+		# Find SQL statement
+		try:
+			return soup.find("pre").text
+		except AttributeError:
+			return None
+
+class insert_SQL:
+
+	def __init__(self, conn):
+		self.conn = conn
+
+	def insert_headers(self, SQLstatement):
+		self.statement = SQLstatement
+		c = self.conn.cursor()
+		c.execute(self.statement)
+		self.conn.commit()
+		return None
+
+class logger:
+
+	def __init__(self, location):
+		self.location = location
+
+	def logMessage(self, loglevel, message):
+		self.loglevel = loglevel
+		self.message = message
+		self.time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+		# Write
+		with open(self.location, 'a') as f:
+			f.write("{}	{}	{}".format(self.time, self.loglevel, self.message))
+			f.write("\n")
+
+# Call
+
+if __name__ == "__main__":
+	# Get file names
+	files = [fileN.replace(".html", "") for fileN in os.listdir(config.path_to_variables)]
+	# Connect to db
+	conn = sqlite3.connect(config.SQL_database_name)
+	# Get sql statements for each file
+	for p in files:
+		print p
+		if "readme" in p:
+			continue
+		# Initiate scraper and scrape
+		scr = scraper(config.path_to_variables + "/" + p + ".html").scrape()
+		# Create SQL tables
+		insert_SQL(conn).insert_headers(scr)
+		# Insert data
+		try:
+			df = pandas.read_csv(config.path_to_data + "/" + p + ".csv")
+			df.to_sql(name = p, con = conn, if_exists = 'append', index=False)
+		except (pandas.parser.CParserError, sqlite3.ProgrammingError, sqlite3.IntegrityError) as error:
+			print "ERROR: {}".format(type(error))
+			log = logger(config.logger_location)
+			log.logMessage("ERROR", "Could not process file {} file due to following error {}".format(p, type(error)))
+			
---
+++ convert_ondemand_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+
+'''
+Config settings
+'''
+
+path_to_data = '/home/jasper/Documents/hadoop_cluster/virtual-hadoop-cluster/data/federalism_data'
+path_to_variables =  '/home/jasper/Documents/hadoop_cluster/virtual-hadoop-cluster/data/federalism_vars'
+SQL_database_name = '/home/jasper/Desktop/federalism_ondemand.db' # Include path if sqlite
+logger_location = '/home/jasper/Desktop/logging.txt'
+
