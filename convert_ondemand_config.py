e65addfb009bd17be15dcc85b2ab0726cf6a9b66
Jasper Ginn <jasperginn@hotmail.com>
2015-11-24 09:20:43 +0100

    Tried with terrorism data

--- convert_ondemand_config.py
+++ convert_ondemand_config.py
@@ -6,10 +6,10 @@ Config settings
 '''
 
 log = True
-log_location = '/home/jasper/Desktop/convert_ondemand/logging.txt'
-path_to_data = '/home/jasper/Documents/hadoop_cluster/virtual-hadoop-cluster/data/federalism_data'
-path_to_variables =  '/home/jasper/Documents/hadoop_cluster/virtual-hadoop-cluster/data/federalism_vars'
-postgres_database_name = 'federalism_ondemand' 
+log_location = '/home/jasper/Desktop/convert_ondemand/logging_terrorism.txt'
+path_to_data = '/home/jasper/Documents/hadoop_cluster/virtual-hadoop-cluster/data/terrorism_data'
+path_to_variables =  '/home/jasper/Documents/hadoop_cluster/virtual-hadoop-cluster/data/terrorism_vars'
+postgres_database_name = 'terrorism_ondemand' 
 postgres_host = '127.0.0.1'
 postgres_user = 'jasper'
 postgres_pwd = 'root'
