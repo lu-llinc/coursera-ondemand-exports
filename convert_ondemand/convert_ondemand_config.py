f280d185abb72a904a87abd1c46f4e629ca49783
Jasper Ginn <jasperginn@hotmail.com>
2016-01-11 12:26:14 +0100

    Added config

---
+++ convert_ondemand/convert_ondemand_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+
+'''
+Config settings
+'''
+
+log = True
+log_location = '/home/jasper/Desktop/logging_terrorism.txt'
+path_to_files = '/home/jasper/Documents/MOOC-DATA/on_demand/federalism_ondemand'
+postgres_database_name = 'terrorism_ondemand' 
+postgres_host = '127.0.0.1'
+postgres_user = 'jasper'
+postgres_pwd = 'root'
