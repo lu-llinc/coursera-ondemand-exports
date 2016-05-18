0b4d990b60b7a60baac4483def54432c985b1a31
Jasper Ginn <jasperginn@hotmail.com>
2016-05-18 13:20:39 +0200

    Update convert_ondemand.py

--- convert_ondemand/convert_ondemand.py
+++ convert_ondemand/convert_ondemand.py
@@ -17,7 +17,7 @@ along with this program.  If not, see [http://www.gnu.org/licenses/].
 
 # Written by: Jasper Ginn
 # Copyright @ Leiden University
-# Last mod: 06-01-2016
+# Last mod: 17-05-2016
 
 '''
 Convert the on-demand course exports to a postgresql table:
